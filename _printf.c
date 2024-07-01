795541fadf5306135814cdf4c3be2efa37ce2478
DeshonS <deshonjewell@gmail.com>
2024-07-01 18:53:54 -0500

    testing my updated code with _putchar

--- _printf.c
+++ _printf.c
@@ -1,53 +1,39 @@
-#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
-/**
- * _printf - formatted output conversion and print data
- * @format: input string
- * Return: numb of chars printed
- */
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
+    const char *p;
 
-int _printf(const char *format, ...)
-{
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char*, unsigned int);
-	char *buffer;
-
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{
-				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{
-				function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+    va_start(args, format);
+    for (p = format; *p != '\0'; p++) {
+        if (*p == '%') {
+            p++;
+            if (*p == 'c') {
+                int c = va_arg(args, int);
+                _putchar(c);
+                count++;
+            } else if (*p == 's') {
+                const char *s = va_arg(args, const char *);
+                while (*s != '\0') {
+                    _putchar(*s);
+                    s++;
+                    count++;
+                }
+            } else if (*p == '%') {
+                _putchar('%');
+                count++;
+            } else {
+                _putchar('%');
+                _putchar(*p);
+                count += 2;
+            }
+        } else {
+            _putchar(*p);
+            count++;
+        }
+    }
+    va_end(args);
+    return count;
 }
