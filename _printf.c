25555df9f222897bb9955c22553422f0a08e90cf
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-07-01 14:02:19 -0500

    err

--- _printf.c
+++ _printf.c
@@ -1,54 +1,53 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+/**
+ * _printf - formatted output conversion and print data
+ * @format: input string
+ * Return: numb of chars printed
+ */
+
+int _printf(const char* format, ...)
 {
-    if (!format)
-        return -1;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char*, unsigned int);
+	char* buffer;
 
-    va_list arguments;
-    va_start(arguments, format);
-
-    char *buffer = malloc(sizeof(char) * 1024);
-    if (!buffer)
-        return -1;
-
-    unsigned int len = 0, ibuf = 0;
-    int (*function)(va_list, char*, unsigned int);
-
-    for (unsigned int i = 0; format[i]; ++i)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '\0')
-            {
-                free(buffer);
-                va_end(arguments);
-                return -1;
-            }
-            function = get_print_func(format, i + 1);
-            if (function == NULL)
-            {
-                if (format[i + 1] == ' ')
-                    return -1;
-                handl_buf(buffer, format[i], ibuf);
-                ++len;
-            }
-            else
-            {
-                len += function(arguments, buffer, ibuf);
-                i += ev_print_func(format, i + 1);
-            }
-            ++i;
-        }
-        else
-        {
-            handl_buf(buffer, format[i], ibuf);
-            ++len;
-        }
-        for (ibuf = len; ibuf > 1024; ibuf -= 1024);
-    }
-    print_buf(buffer, ibuf);
-    free(buffer);
-    va_end(arguments);
-    return len;
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{
+				function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes
+ * Return: 1
+ */
+
+int _putchar()
+{
+
+}
