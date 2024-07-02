24908947d72311b46f7b2cb7114edb0a66931933
DeshonS <deshonjewell@gmail.com>
2024-07-01 20:48:46 -0500

    made some updates

--- _printf.c
+++ _printf.c
@@ -4,19 +4,19 @@
 
 int _printf(const char *format, ...) {
     va_list args;
-    int count = 0;
-    const char *p;
+    int count = 0, c;
+    const char *p, *s;
 
     va_start(args, format);
     for (p = format; *p != '\0'; p++) {
         if (*p == '%') {
             p++;
             if (*p == 'c') {
-                int c = va_arg(args, int);
+                c = va_arg(args, int);
                 _putchar(c);
                 count++;
             } else if (*p == 's') {
-                const char *s = va_arg(args, const char *);
+                s = va_arg(args, const char *);
                 while (*s != '\0') {
                     _putchar(*s);
                     s++;
